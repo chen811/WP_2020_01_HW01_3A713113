08e6d406284d1dc50d685ec7b05dec3001aa7267
user <user@DESKTOP-7L4BJMM>
2020-11-12 20:37:29 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW01_3A713113/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WP_2020_01_HW01_3A713113
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tex = new System.Windows.Forms.TextBox();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(549, 219);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(126, 98);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "轉換";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // tex
+            // 
+            this.tex.Location = new System.Drawing.Point(487, 147);
+            this.tex.Multiline = true;
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(249, 66);
+            this.tex.TabIndex = 1;
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(378, 401);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(472, 96);
+            this.rtb1.TabIndex = 2;
+            this.rtb1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體-ExtB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(466, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "請將需要轉換的字串輸入:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(557, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "轉換結果:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 723);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tex;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WP_2020_01_HW01_3A713113/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+
+namespace WP_2020_01_HW01_3A713113
+{
+    public partial class Form1 : Form
+    {
+       // MD5 md5 = MD5.Create();//建立一個MD5
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string input_str = tex.Text;
+            //轉換int 
+            //int input_int = int.Parse(input_str);
+
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            byte[] source = Encoding.Default.GetBytes(tex.Text);//將字串轉為Byte[]
+            byte[] crypto = md5.ComputeHash(source);//進行MD5加密
+            string result = Convert.ToBase64String(crypto);//把加密後的字串從Byte[]轉為字串
+            //Response.Write("MD5加密:  " + result);//輸出結果
+            rtb1.Text= "輸出結果:  " + result;//輸出結果
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
